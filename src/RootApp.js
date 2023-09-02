269d3bd5804c9545e5867db84bc92193386af83c
dev1week <rondo2860@gmail.com>
2023-09-02 14:01:14 +0900

    #6 -1 Splash 적용

--- src/RootApp.js
+++ src/RootApp.js
@@ -1,7 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { RootStackNavigation } from "./navigations/RootStackNavigation";
+import { SplashView } from "../SplashView";
 export const RootApp = () => {
+  const [initialized, setInitialized] = useState(false);
+  if (!initialized) {
+    return (
+      <SplashView
+        onFinishLoad={() => {
+          setInitialized(true);
+        }}
+      />
+    );
+  }
   return (
     <NavigationContainer>
       <RootStackNavigation></RootStackNavigation>
